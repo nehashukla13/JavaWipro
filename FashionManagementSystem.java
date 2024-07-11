3038c2790c6002512c3f41ff3a6cec6088b78c12
nehashukla13 <114692830+nehashukla13@users.noreply.github.com>
2024-07-11 20:26:40 +0530

    FashionManagementSystem.java

--- FashionManagementSystem.java
+++ FashionManagementSystem.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 
-// Interface for clothing items
 interface ClothingItem {
     String getType();   // Method to get type of clothing item
     String getColor();  // Method to get color of clothing item
 }
 
-// Class for tops
 class Top implements ClothingItem {
     private String type;
     private String color;
@@ -31,7 +29,6 @@ class Top implements ClothingItem {
     }
 }
 
-// Class for bottoms
 class Bottom implements ClothingItem {
     private String type;
     private String color;
@@ -56,22 +53,18 @@ class Bottom implements ClothingItem {
     }
 }
 
-// Main class to manage outfits
 public class FashionManagementSystem {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input for top
         System.out.print("Enter color for top: ");
         String topColor = scanner.nextLine();
         ClothingItem top = new Top(topColor);
 
-        // Input for bottom
         System.out.print("Enter color for bottom: ");
         String bottomColor = scanner.nextLine();
         ClothingItem bottom = new Bottom(bottomColor);
 
-        // Display the outfit
         System.out.println("\nOutfit:");
         System.out.println("Top: " + top);
         System.out.println("Bottom: " + bottom);
